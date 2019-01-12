7a64dbf1a87e08edb49eaad37b3122a0bd100748
Damian Nolan <damiannolan@gmail.com>
2019-01-12 22:45:17 +0000

    removed formulae from brew

--- install/brew.sh
+++ install/brew.sh
@@ -1,69 +1,16 @@
 #!/bin/sh
 
-# cli tools
 brew install curl
 brew install ack
 brew install the_silver_searcher
 brew install tree
 brew install awscli
-brew install heroku-toolbelt
-brew install unrar
-
-# development server setup
-brew install nginx
-#brew install dnsmasq
-brew install mongodb
-brew install couchdb
-brew install redis 
-brew install postgresql
-brew install postgis
-brew install pgcli
-
-# development tools
-brew install git
-brew install git-flow
-#brew install hub
 brew install macvim --with-override-system-vim
 brew install reattach-to-user-namespace
 brew install tmux
 brew install zsh
-# Highlight looks useful but may never use
-brew install highlight
 brew install nvm
-brew install z
-brew install markdown
-brew install jenv
-brew install python
-brew install python3
-brew install octave
-
-# Utils
-brew install automake
-brew install readline
-brew install nmap
-brew install imagemagick
-brew install jpeg
-brew install libpng
-brew install openssl
 brew install yarn
-
-# Homebrew Cask for command line installation of GUI tools
-brew install caskroom/cask/brew-cask
-
-brew cask install google-chrome
-brew cask install firefox
-brew cask install opera
-brew cask install virtualbox
-brew cask install dockertoolbox
-brew cask install spotify
-brew cask install iterm2
-brew cask install vlc
-brew cask install brackets
-brew cask install easysimbl
-
-brew tap caskroom/versions
-brew cask install java7
 brew cask install java
 brew install maven
-brew install leiningen
 
